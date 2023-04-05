854cdc25caa76603c05c9b7737733d31178d2e7d
BigBadE <bigbade333@gmail.com>
2023-04-04 22:55:42 -0500

    Added tokenizer

--- src/main/java/bigbade/raven/ravenintellijplugin/natives/ParserRef.java
+++
@@ -1,9 +0,0 @@
-package bigbade.raven.ravenintellijplugin.natives;
-
-public class ParserRef {
-    public long reference;
-
-    public ParserRef(long inReference) {
-        reference = inReference;
-    }
-}
